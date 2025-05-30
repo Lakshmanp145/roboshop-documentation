0e3b6dd80c775b26c57c1402a2b9514e8517d622
Lakshmanp1 <laxman.gdn@gmail.com>
2025-05-30 16:53:22 +0530

    updated Route53 details

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z021448929NMXW4P65UQE" # replace with your ZONE ID
-DOMAIN_NAME="daws82s.online" # replace with your domain
+ZONE_ID="Z04042331YAVAJS5WAC3G" # replace with your ZONE ID
+DOMAIN_NAME="lakshman.site" # replace with your domain
 
 for instance in ${INSTANCES[@]}
 #for instance in $@
